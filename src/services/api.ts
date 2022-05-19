62528d4ba31552923b3ed871213da46bdbf89121
Matheus Lima Ribeiro <86062294+mdmath15@users.noreply.github.com>
2022-05-19 09:16:15 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const getAdress = async (cep: string): any => {
+export const getAdress = async (cep: string) => {
     const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
     return response.data
 }
